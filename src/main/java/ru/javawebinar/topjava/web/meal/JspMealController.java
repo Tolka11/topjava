703cf822c9748e2507c7665496c61adeade38596
Anatoliy Skrylnikov <anatoliy.skrylnikov@gmail.com>
2021-07-12 20:57:28 +0600

    HW6

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -5,16 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
-import ru.javawebinar.topjava.service.UserService;
-import ru.javawebinar.topjava.web.SecurityUtil;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -26,88 +24,66 @@ import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
 public class JspMealController extends AbstractMealController {
-    public JspMealController(MealService service) {
+
+    public JspMealController(@Autowired MealService service) {
         super(service);
     }
 
-//    //*******************************************
-//
-//    @GetMapping("/")
-//    public String root() {
-//        return "index";
-//    }
-//
-//    @GetMapping("/users")
-//    public String getUsers(Model model) {
-//        model.addAttribute("users", service.getAll());
-//        return "users";
-//    }
-//
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        String action = request.getParameter("action");
-//
-//        switch (action == null ? "all" : action) {
-//            case "delete" -> {
-//                int id = getId(request);
-//                mealController.delete(id);
-//                response.sendRedirect("meals");
-//            }
-//            case "create", "update" -> {
-//                final Meal meal = "create".equals(action) ?
-//                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-//                        mealController.get(getId(request));
-//                request.setAttribute("meal", meal);
-//                request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
-//            }
-//            case "filter" -> {
-//                LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
-//                LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
-//                LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
-//                LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
-//                request.setAttribute("meals", mealController.getBetween(startDate, startTime, endDate, endTime));
-//                request.getRequestDispatcher("/meals.jsp").forward(request, response);
-//            }
-//            default -> {
-//                request.setAttribute("meals", mealController.getAll());
-//                request.getRequestDispatcher("/meals.jsp").forward(request, response);
-//            }
-//        }
-//    }
-//
-//    //*******************************************
-//
-//    @PostMapping("/users")
-//    public String setUser(HttpServletRequest request) {
-//        int userId = Integer.parseInt(request.getParameter("userId"));
-//        SecurityUtil.setAuthUserId(userId);
-//        return "redirect:meals";
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.setCharacterEncoding("UTF-8");
-//        Meal meal = new Meal(
-//                LocalDateTime.parse(request.getParameter("dateTime")),
-//                request.getParameter("description"),
-//                Integer.parseInt(request.getParameter("calories")));
-//
-//        if (StringUtils.hasLength(request.getParameter("id"))) {
-//            mealController.update(meal, getId(request));
-//        } else {
-//            mealController.create(meal);
-//        }
-//        response.sendRedirect("meals");
-//    }
+    @GetMapping(value = "/meals")
+    public String getAllMeals(Model model) {
+        model.addAttribute("meals", getAll());
+        return "meals";
+    }
 
-    //*******************************************
+    @GetMapping(value = "/updatemeal/{id}")
+    public String updateMeal(Model model, @PathVariable int id) {
+        model.addAttribute("meal", get(id));
+        return "mealForm";
+    }
 
+    @GetMapping(value = "/addmeal")
+    public String addMeal(Model model) {
+        model.addAttribute("meal", new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
+        return "mealForm";
+    }
 
+    @GetMapping(value = "/deletemeal/{id}")
+    public String deleteMeal(@PathVariable int id) {
+        delete(id);
+        return "redirect:/meals";
+    }
 
+    @GetMapping(value = "/meals/filter")
+    public String getFilteredMeals(Model model,
+                                   @RequestParam(value = "startDate", required = false) String startDate,
+                                   @RequestParam(value = "endDate", required = false) String endDate,
+                                   @RequestParam(value = "startTime", required = false) String startTime,
+                                   @RequestParam(value = "endTime", required = false) String endTime) {
+        System.out.println(startDate + " " + endDate + " " + startTime + " " + endTime);
+        LocalDate sDate = parseLocalDate(startDate);
+        LocalDate eDate = parseLocalDate(endDate);
+        LocalTime sTime = parseLocalTime(startTime);
+        LocalTime eTime = parseLocalTime(endTime);
+        model.addAttribute("meals", getBetween(sDate, sTime, eDate, eTime));
+        return "meals";
+    }
+
+    @PostMapping({"/savemeal", "/updatemeal/savemeal"})
+    public String createMeal(HttpServletRequest request) throws UnsupportedEncodingException {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+        if (StringUtils.hasLength(request.getParameter("id"))) {
+            update(meal, getId(request));
+        } else {
+            create(meal);
+        }
+        return "redirect:/meals";
+    }
 
     private int getId(HttpServletRequest request) {
         String paramId = Objects.requireNonNull(request.getParameter("id"));
         return Integer.parseInt(paramId);
     }
-
 }
